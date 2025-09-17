8bfddfa50744f32f076a932e03e4842c945c145f
Your Name <you@example.com>
2025-09-17 11:28:42 -0500

    completed

--- Step3/commandMotorsfromRpi.py
+++ Step3/commandMotorsfromRpi.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from sendStringScript import sendString
 
-String2Send='<150,10>'  #these are my motor commands
+String2Send='<150,150>'  #these are my motor commands
 
 if __name__ == '__main__':
     ser=serial.Serial("/dev/ttyACM0",115200)
